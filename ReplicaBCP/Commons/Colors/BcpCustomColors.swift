73b055dcfc01832aaad504de88ed03de13925721
Moises Almeyda <moisesalmeyda@bcp.com.pe>
2021-04-30 23:00:44 -0500

    folder

---
+++ ReplicaBCP/Commons/Colors/BcpCustomColors.swift
@@ -0,0 +1,14 @@
+//
+//  BcpColors.swift
+//  ReplicaBCP
+//
+//  Created by Moises Almeyda on 25/04/21.
+//
+
+import UIKit
+
+enum BcpCustomColors {
+    
+    static let BlueBcpPrimary = UIColor(named: "BcpPrimaryColor")
+    static let OrangeBcpPrimary = UIColor(named: "BcpSecundaryColor")
+}
